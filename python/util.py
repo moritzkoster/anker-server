fa22ac477522f756cd68d12100c77eee07401383
moritzkoster <moritz.koster@bluewin.ch>
2021-05-12 21:08:37 +0200

    promotion finder for coop

--- python/util.py
+++ python/util.py
@@ -2,8 +2,6 @@ from datetime import date
 
 cachelist = {}
 
-#last = {"anker": {"date": 0, "info": ""}}
-
 def cache1d(function):
     def wrapper(name):
         if name not in cachelist:
@@ -13,18 +11,8 @@ def cache1d(function):
             return cachelist[name]["info"]
         else:
             info = function(name)
-            #info = "this strange shit"
             cachelist[name]["info"] = info
             cachelist[name]["date"] = date.today()
             print("new request")
             return info
     return wrapper
-
-#
-# def cachelist():
-#     cachelist = {}
-#     for element in list:
-#         cachelist[element] = {"date": 0, "info": ""}
-#     return cachelist
-
-#cachelist = cachelist()
--- server.py
+++ server.py
@@ -2,31 +2,32 @@ from flask import Flask, request, render_template, send_from_directory
 import json
 import os
 from flask_cors import CORS
-import python.util as util
+import requests
+from bs4 import BeautifulSoup
 from datetime import date
 
-global last
+import python.util as util
 
-# def cache1d(function):
-#     def wrapper(name):
-#         if last[name]["date"] == date.today():
-#             return last[name]["info"]
-#             print("already existed")
-#         else:
-#             info = function(name)
-#             last[name]["info"] = info
-#             last[name]["date"] = date.today()
-#             print("new request")
-#             return info
-#     return wrapper
+app = app = Flask(__name__)
+
+coop = {"anker": {"url": 'https://www.coop.ch/de/lebensmittel/getraenke/bier/multipacks-ab-12x50cl/anker-lager-bier-24x50cl/p/3458809', "attr_key": "data-testauto", "attr_value": "productlistpromotion3458809"}}
+
+@app.route("/anker")
+def anker():
+    return coop_info("anker")
 
 @util.cache1d
-def get_info(name):
-    return "the holy Value"
+def coop_info(name):
+    vgm_url = coop[name]["url"]
+    html_text = requests.get(vgm_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
 
+    promotion_field= soup.find(attrs={coop[name]["attr_key"]: coop[name]["attr_value"]})
+    if promotion_field == None:
+        return "kei Aktion"
+    elif "53%" in promotion_field.get_text():
+        return "Anker Halbe Priis"
+    return "Anker aktion aber ned halbe priis."
 
-#last = {"anker": {"date": 0, "info": ""}}
-
-print(get_info("anker"))
-#last["anker"]["info"] = "ha di verwütscht"
-print(get_info("anker"))
+if __name__ == "__main__":
+    app.run()
